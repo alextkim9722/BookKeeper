d6f48d85c800dba8123f8c1feca273147308a4f6
alextkim9722 <82482033+alextkim9722@users.noreply.github.com>
2024-08-25 22:42:20 -0700

    Setting Up React
    - Recreated react project as I reset my entire computer a few days prior
    - Set up small elements to cordon profile and shelf sections

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -4,11 +4,11 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
+      {/* <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -27,7 +27,9 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+      </p> */}
+      <div id="ProfileColumn">profile</div>
+      <div id="ShelfColumn">shelf</div>
     </>
   )
 }
