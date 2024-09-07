bc6fd73d89b70b200519f3498e016a7ffc33f5ec
alextkim9722 <82482033+alextkim9722@users.noreply.github.com>
2024-09-07 12:44:37 -0700

    Text fields pop up to change user data | No post request has been made yet

--- FrontEnd/src/components/Profile/Profile.jsx
+++ FrontEnd/src/components/Profile/Profile.jsx
@@ -1,12 +1,29 @@
+import { useState } from 'react';
+
 import styles from './Profile.module.css'
 import globalStyles from '../../Global.module.css'
 
 export function Profile (props)
 {
+    const [editMode, setEditMode] = useState(false);
+    const [userNameCharCount, setUserNameCharCount] = useState(0);
+
+    const MAX_USERNAME_CHAR_COUNT = 25;
+
     return(
         <div id={styles.profileRoot} className={`${globalStyles.pad}`} style={{'--padding':'30px', '--width':'30%', color:'black'}}>
             <img id={styles.profilePicture} className={`${globalStyles.round} ${globalStyles.center}`} src="/src/assets/default.png" />
-            <h3> {props.user.username} </h3>
+
+            {!editMode && <h3> {props.user.username} </h3>}
+            {editMode && (
+                <>
+                <div style={{width:'100%', margin:'20px auto'}} >
+                    <input style={{width:'100%'}} type='text' onChange={(event) => setUserNameCharCount(event.target.value.length)} />
+                    <div style={{float:'right'}}>{userNameCharCount}/{MAX_USERNAME_CHAR_COUNT}</div>
+                </div>
+                </>
+            )}
+
             <table>
                 <tbody>
                     <tr>
@@ -23,9 +40,11 @@ export function Profile (props)
                     </tr>
                 </tbody>
             </table>
-            <div>
-                <p style={{flexGrow:'1', height:'200px', textAlign:'justify', wordBreak:'break-word'}}>{props.user.description}</p>
-            </div>
+            {!editMode && <p style={{width:'100%', height:'200px', textAlign:'justify', wordBreak:'break-word'}}>{props.user.description}</p>}
+            {editMode && <textarea style={{width:'100%', height:'200px', margin:'10px auto'}}/>}
+
+            {!editMode && <div className={`${globalStyles.interactible}`} onClick={() => setEditMode(true)}>Edit Profile</div>}
+            {editMode && <div className={`${globalStyles.interactible}`} onClick={() => setEditMode(false)}>Confirm</div>}
         </div>
     )
 }
--- FrontEnd/src/components/ProfileShelf.jsx
+++ FrontEnd/src/components/ProfileShelf.jsx
@@ -46,6 +46,7 @@ export default function ProfileShelf()
         })
         .catch(err => console.error(err))
     }, [])
+    
 
     return (
         <>
